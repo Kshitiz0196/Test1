c5e3eb6e147b83e2d8d6417357aaed01785419cf
Kshitiz0196 <Kshitiz0196@gmai.com>
2020-05-30 19:13:19 +0530

    commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.String;
+
+class Test
+{
+
+static int stringCalc(String a, String b)
+{
+ int x,y=0;
+ if(a.isEmpty()||b.isEmpty())
+return 0;
+
+else
+{
+  x=(int)a;
+  y=(int)b;
+}
+  }
+}
